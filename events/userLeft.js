ea3bdc40a2ba6fea07f98113491546b83350768e
eribloot <Fenton_Eric@pm.me>
2022-05-26 00:52:09 -0500

    revert some changes

--- events/userLeft.js
+++ events/userLeft.js
@@ -3,7 +3,6 @@ const { bold, userMention } = require("@discordjs/builders");
 const { MessageEmbed }  = require("discord.js");
 
 const { Users } = require("../data/dbObjects.js");
-const { Channels } = require("../utils/getChannels.js");
 
 module.exports = {
   name: "guildMemberRemove",
@@ -13,8 +12,7 @@ module.exports = {
       const username = member.user.tag;
       
       //get log channel
-      const logChannel = Channels.get("joins-and-leaves");
-
+      const logChannel = await member.guild.channels.cache.get("742187165659693076");
       //leave log message
       const leaveLog = new MessageEmbed()
       .setColor("DARK_BUT_NOT_BLACK")
